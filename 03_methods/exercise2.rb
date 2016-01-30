1f824ec195553089ab52ad0ae55f3278af1f002d
TWHou <hsiehkunlun@hotmail.com>
2016-01-30 11:12:15 +0000

    Renamed folders for order. Add exercises for methods

---
+++ 03_methods/exercise2.rb
@@ -0,0 +1,16 @@
+# What do the following expressions evaluate to?
+
+# 1. x = 2
+puts " => 2"
+
+# 2. puts x = 2
+puts " => nil"
+
+# 3. p name = "Joe"
+puts ' => "Joe"'
+
+# 4. four = "four"
+puts ' => "four"'
+
+# 5. print something = "nothing"
+puts " => nil"
---
+++ 03_methods/exercise5.rb
@@ -0,0 +1,10 @@
+# What will the following code print to the screen?
+
+def scream(words)
+ words = words + "!!!!"
+ puts words
+end
+
+scream("Yippeee")
+
+puts "This code prints \"Yippeee!!!!\" to the screen. But it returns nil."
